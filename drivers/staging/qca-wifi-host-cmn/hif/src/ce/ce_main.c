f61da25e7be91be84ce7a39b6f689eeb7157b26f
Madara273 <ravenhoxs@gmail.com>
2025-11-03 19:47:34 +0000

    hif: cast A_STATUS to QDF_STATUS in hif_send_head to fix compiler warning

--- drivers/staging/qca-wifi-host-cmn/hif/src/ce/ce_main.c
+++ drivers/staging/qca-wifi-host-cmn/hif/src/ce/ce_main.c
@@ -2227,7 +2227,11 @@ hif_send_head(struct hif_opaque_softc *hif_ctx,
 
 	if (qdf_unlikely(!ce_hdl)) {
 		hif_err("CE handle is null");
+<<<<<<< HEAD
 		return QDF_STATUS_E_FAILURE;
+=======
+		return (QDF_STATUS)A_ERROR;
+>>>>>>> a05f38377aa4 (hif: cast A_STATUS to QDF_STATUS in hif_send_head to fix compiler warning)
 	}
 
 	QDF_NBUF_UPDATE_TX_PKT_COUNT(nbuf, QDF_NBUF_TX_PKT_HIF);
