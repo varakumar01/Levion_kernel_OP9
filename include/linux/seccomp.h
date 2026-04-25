e6bdf968401e4e329fee87b3aed26bd7bdb7b800
Vara kumar <varakumar7000@gmail.com>
2026-04-25 11:08:28 +0530

    revert: removed duplicate entries from the commit: a2356d8a618fa3e75596aceec2ca186b4ec69c92

--- include/linux/seccomp.h
+++ include/linux/seccomp.h
@@ -12,7 +12,6 @@
 #ifdef CONFIG_SECCOMP
 
 #include <linux/thread_info.h>
-#include <linux/atomic.h>
 #include <asm/seccomp.h>
 
 struct seccomp_filter;
@@ -29,7 +28,6 @@ struct seccomp_filter;
  */
 struct seccomp {
 	int mode;
-	atomic_t filter_count;
 	struct seccomp_filter *filter;
 };
 
