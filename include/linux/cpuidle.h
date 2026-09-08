b6b5c0ae59572e5afbd51113e7a67c6bfec83eb9
Varakumar <varakumar7000@gmail.com>
2026-09-09 00:03:25 +0530

    Revert "[cpuidle] reject idle entry when need_resched() is already true"
    This reverts commit 3f248d175bc03f461eed81aaec4be877ef8d5b63.
    
    Signed-off-by: Varakumar <varakumar7000@gmail.com>

--- include/linux/cpuidle.h
+++ include/linux/cpuidle.h
@@ -14,7 +14,6 @@
 #include <linux/percpu.h>
 #include <linux/list.h>
 #include <linux/hrtimer.h>
-#include <linux/sched.h>
 
 #define CPUIDLE_STATE_MAX	10
 #define CPUIDLE_NAME_LEN	16
@@ -280,18 +279,17 @@ static inline int cpuidle_register_governor(struct cpuidle_governor *gov)
 ({									\
 	int __ret = 0;							\
 									\
-	if (need_resched()) {						\
-		__ret = -1;						\
-	} else if (!idx) {						\
+	if (!idx) {							\
 		cpu_do_idle();						\
-	} else {							\
+		return idx;						\
+	}								\
+									\
+	if (!is_retention)						\
+		__ret =  cpu_pm_enter();				\
+	if (!__ret) {							\
+		__ret = low_level_idle_enter(state);			\
 		if (!is_retention)					\
-			__ret = cpu_pm_enter();				\
-		if (!__ret) {						\
-			__ret = low_level_idle_enter(state);		\
-			if (!is_retention)				\
-				cpu_pm_exit();				\
-		}							\
+			cpu_pm_exit();					\
 	}								\
 									\
 	__ret ? -1 : idx;						\
